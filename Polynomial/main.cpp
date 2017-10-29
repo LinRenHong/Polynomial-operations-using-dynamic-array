bfc05e21166976be71800b7f7d0a452c5a3af0ee
林仁鴻 <linrenhong@linrenhongde-MacBook-Pro.local>
2017-10-29 22:00:27 +0800

    Initial Commit

---
+++ Polynomial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Polynomial
+//
+//  Created by 林仁鴻 on 2017/10/29.
+//  Copyright © 2017年 林仁鴻. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
